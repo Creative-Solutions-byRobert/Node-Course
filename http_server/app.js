2ba915ce312f9948ca7efe72acdfd2a59d655904
Creative-Solutions-byRobert <robertbourton777@gmail.com>
2022-05-29 06:38:53 -0400

    One liner

--- http_server/app.js
+++ http_server/app.js
@@ -2,7 +2,5 @@ const http = require( 'http' )
 
 const routes = require('./router')
 
-const server = http.createServer(routes) 
-
-
-server.listen( 3000, () => console.log( 'Server has started and is listening on port 3000.\nGo to http://localhost:3000' ) )
+const server = http.createServer(routes)
+				.listen( 3000, () => console.log( 'Server has started and is listening on port 3000.\nGo to http://localhost:3000' ) )
